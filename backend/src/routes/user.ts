46722f8ae0025ff4aed538ca1fec94644bfa6322
ronak <ronakpraja340@gmail.com>
2026-03-19 00:18:54 +0530

    debugs

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,63 +3,63 @@ import { sign, verify } from 'hono/jwt';
 import { drizzle } from 'drizzle-orm/d1';
 import { eq } from 'drizzle-orm';
 import { users } from '../db/schema';
-import { signupInput,signinInput } from "@ronak3333/medium-common"
+import { signupInput, signinInput } from "@ronak3333/medium-common"
 
 export const userRouter = new Hono<{
-    Bindings: {
-      medium : D1Database
-      JWT_SECRET : string
-    }
-  }>();
+  Bindings: {
+    medium: D1Database
+    JWT_SECRET: string
+  }
+}>();
 
 // signup route
-userRouter.post("/signup",async (c)=>{
-  
-    const db = drizzle(c.env.medium);
-    
-    const body = await c.req.json()
-    const {success} = signupInput.safeParse(body)
-    if(!success){
-      c.status(400)
-      return c.json({error : "Invalid input"})
-    }
-    try {
-      const result = await db.insert(users).values({
-        id: crypto.randomUUID(),
-        email : body.email,
-        password : body.password,
-        name : body.name
-      }).returning({ id: users.id });
-      
-      const user = result[0];
-      
-      const jwt = await sign({id : user.id},c.env.JWT_SECRET)
+userRouter.post("/signup", async (c) => {
 
-      return c.json(jwt)
-      
-    } catch (error) {
-      c.status(403)
-      return c.json({message: "User already exists/error while creating user"})
-    }
-  })
-  
-  // signin route
-  userRouter.post("/signin",async (c)=>{
-    const db = drizzle(c.env.medium);
-    
-    const body = await c.req.json()
-    const {success} = signinInput.safeParse(body)
-    if(!success){
-      c.status(400)
-      return c.json({error : "Invalid input"})
-    }
-    const result = await db.select().from(users).where(eq(users.email, body.email));
+  const db = drizzle(c.env.medium);
+
+  const body = await c.req.json()
+  const { success } = signupInput.safeParse(body)
+  if (!success) {
+    c.status(400)
+    return c.json({ error: "Invalid input" })
+  }
+  try {
+    const result = await db.insert(users).values({
+      id: crypto.randomUUID(),
+      email: body.email,
+      password: body.password,
+      name: body.name
+    }).returning({ id: users.id });
+
     const user = result[0];
-    if(!user){
-      c.status(403)
-      return c.json({error : "user not found"})
-    }
-    
-    const jwt = await sign({id : user.id},c.env.JWT_SECRET)
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+
     return c.json(jwt)
-  })
+
+  } catch (error) {
+    c.status(403)
+    return c.json({ message: "User already exists/error while creating user", error })
+  }
+})
+
+// signin route
+userRouter.post("/signin", async (c) => {
+  const db = drizzle(c.env.medium);
+
+  const body = await c.req.json()
+  const { success } = signinInput.safeParse(body)
+  if (!success) {
+    c.status(400)
+    return c.json({ error: "Invalid input" })
+  }
+  const result = await db.select().from(users).where(eq(users.email, body.email));
+  const user = result[0];
+  if (!user) {
+    c.status(403)
+    return c.json({ error: "user not found" })
+  }
+
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+  return c.json(jwt)
+})
